a9850ebd093cfb5faa6788fa2c7c0887abc008f1
Abbosbek <adev0197@gmail.com>
2023-02-07 15:36:52 +0500

    Contact_new_view

--- app/src/main/java/uz/abbosbek/contact_video/MainActivity.kt
+++ app/src/main/java/uz/abbosbek/contact_video/MainActivity.kt
@@ -1,17 +1,24 @@
 package uz.abbosbek.contact_video
 
 import android.Manifest
+import android.content.ContentProviderOperation
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.coroutines.delay
 import uz.abbosbek.contact_video.databinding.ActivityMainBinding
+import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -40,15 +47,188 @@ class MainActivity : AppCompatActivity() {
 
         binding.saveFab.setOnClickListener {
             /** first check is permission allowed or not */
-            if (isWriteContactPermissionEnabled()){
+            if (isWriteContactPermissionEnabled()) {
                 /** permission already granted, save contact*/
                 saveContact()
+            } else {
+                /** permission was not granted, save contact*/
+                requestWriteCintactPermission()
             }
         }
     }
 
     private fun saveContact() {
+        Log.d(TAG, "sevContact: ")
+        /** input data */
+        val firstName = binding.firstNameEt.text.toString().trim()
+        val lastName = binding.lastNameEt.text.toString().trim()
+        val phoneMobile = binding.phoneMobileEt.text.toString().trim()
+        val phoneHome = binding.phoneHomeEt.text.toString().trim()
+        val email = binding.emailEt.text.toString().trim()
+        val address = binding.addressEt.text.toString().trim()
 
+        Log.d(TAG, "saveContact: First Name $firstName")
+        Log.d(TAG, "saveContact: Last Name $lastName")
+        Log.d(TAG, "saveContact: Phone Mobile  $phoneMobile")
+        Log.d(TAG, "saveContact: Phone Home $phoneHome")
+        Log.d(TAG, "saveContact: Email $email")
+        Log.d(TAG, "saveContact: Address $address")
+
+        /** init arraylist of object ContentProvideOperation */
+        val cpo = ArrayList<ContentProviderOperation>()
+
+        /** contact id */
+        val rawContactId = cpo.size
+        cpo.add(
+            ContentProviderOperation.newInsert(
+                ContactsContract.RawContacts.CONTENT_URI
+            )
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                .build()
+        )
+
+        /** Add first name, last name */
+        cpo.add(
+            ContentProviderOperation.newInsert(
+                ContactsContract.Data.CONTENT_URI
+            )
+                .withValue(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, rawContactId)
+                .withValue(
+                    ContactsContract.RawContacts.Data.MIMETYPE,
+                    ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE
+                )
+                .withValue(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, firstName)
+                .withValue(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME, lastName)
+                .build()
+        )
+
+        /** add phone number (Mobile) */
+        cpo.add(
+            ContentProviderOperation.newInsert(
+                ContactsContract.Data.CONTENT_URI
+            )
+                .withValue(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, rawContactId)
+                .withValue(
+                    ContactsContract.RawContacts.Data.MIMETYPE,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE
+                )
+                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, phoneMobile)
+                .withValue(
+                    ContactsContract.CommonDataKinds.Phone.TYPE,
+                    ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE
+                )
+                .build()
+        )
+
+        /** add phone number (home) */
+        cpo.add(
+            ContentProviderOperation.newInsert(
+                ContactsContract.Data.CONTENT_URI
+            )
+                .withValue(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, rawContactId)
+                .withValue(
+                    ContactsContract.RawContacts.Data.MIMETYPE,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE
+                )
+                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, phoneHome)
+                .withValue(
+                    ContactsContract.CommonDataKinds.Phone.TYPE,
+                    ContactsContract.CommonDataKinds.Phone.TYPE_HOME
+                )
+                .build()
+        )
+
+        /** add email */
+        cpo.add(
+            ContentProviderOperation.newInsert(
+                ContactsContract.Data.CONTENT_URI
+            )
+                .withValue(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, rawContactId)
+                .withValue(
+                    ContactsContract.RawContacts.Data.MIMETYPE,
+                    ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE
+                )
+                .withValue(ContactsContract.CommonDataKinds.Email.DATA, email)
+                .withValue(
+                    ContactsContract.CommonDataKinds.Email.TYPE,
+                    ContactsContract.CommonDataKinds.Email.TYPE_WORK
+                )
+                .build()
+        )
+
+        /** add address */
+        cpo.add(
+            ContentProviderOperation.newInsert(
+                ContactsContract.Data.CONTENT_URI
+            )
+                .withValue(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, rawContactId)
+                .withValue(
+                    ContactsContract.RawContacts.Data.MIMETYPE,
+                    ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE
+                )
+                .withValue(ContactsContract.CommonDataKinds.StructuredPostal.DATA, address)
+                .withValue(
+                    ContactsContract.CommonDataKinds.StructuredPostal.TYPE,
+                    ContactsContract.CommonDataKinds.StructuredPostal.TYPE_WORK
+                )
+                .build()
+        )
+
+        /** get image as bytes array to save as contact image */
+        val imageBytes = imageUriToBytes()
+        if (imageBytes != null) {
+            /** contact with image */
+            Log.d(TAG, "saveContact: contact with image ")
+            /** add image */
+            cpo.add(
+                ContentProviderOperation.newInsert(
+                    ContactsContract.Data.CONTENT_URI
+                )
+                    .withValue(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, rawContactId)
+                    .withValue(
+                        ContactsContract.RawContacts.Data.MIMETYPE,
+                        ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE
+                    )
+                    .withValue(ContactsContract.CommonDataKinds.Photo.PHOTO, imageBytes)
+                    .build()
+            )
+
+        } else {
+            /** contact without image */
+            Log.d(TAG, "saveContact: contact without image ")
+        }
+
+        /** save contact */
+        try {
+            contentResolver.applyBatch(ContactsContract.AUTHORITY, cpo)
+            Log.d(TAG, "saveContact: saved")
+            Toast.makeText(this, "saved", Toast.LENGTH_SHORT).show()
+
+        } catch (e: Exception) {
+            Log.d(TAG, "saveContact: failed to save due to ${e.message}")
+            Toast.makeText(this, "failed to save due to ${e.message}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun imageUriToBytes(): ByteArray? {
+        val bitmap: Bitmap
+        val baos: ByteArrayOutputStream
+
+        return try {
+            if (Build.VERSION.SDK_INT < 28) {
+                bitmap = MediaStore.Images.Media.getBitmap(contentResolver, image_uri)
+            } else {
+                val source = ImageDecoder.createSource(contentResolver, image_uri!!)
+                bitmap = ImageDecoder.decodeBitmap(source)
+            }
+            baos = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos)
+            baos.toByteArray()
+        } catch (e: Exception) {
+            Log.d(TAG, "imageUriToBytes: ${e.message}")
+            null
+        }
     }
 
     private fun isWriteContactPermissionEnabled(): Boolean {
@@ -77,12 +257,13 @@ class MainActivity : AppCompatActivity() {
         /** handle permission request result */
         if (grantResults.isNotEmpty()) {
             if (requestCode == WRITE_CONTACT_PERMISSION_CODE) {
-                val haveWriteContactPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED /** true if granted, false if not */
-                if (haveWriteContactPermission){
+                val haveWriteContactPermission =
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+                /** true if granted, false if not */
+                if (haveWriteContactPermission) {
                     /** permission granted, save contact*/
                     saveContact()
-                }
-                else{
+                } else {
                     /**permission denied, can't save contact */
                     Toast.makeText(this, "Permission dened", Toast.LENGTH_SHORT).show()
                 }
